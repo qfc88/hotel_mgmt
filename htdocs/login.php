c7bdfd0a98672e254039791b9342387be1a4c434
TommyKhang <93020409+TommyKhang@users.noreply.github.com>
2024-05-27 08:49:28 +0700

    Add files via upload
    update

--- htdocs/login.php
+++ htdocs/login.php
@@ -2,16 +2,17 @@
 
 include 'config.php';
 session_start();
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- sweet alert -->
@@ -20,56 +21,67 @@ session_start();
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <!-- loading bar -->
     <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
-    <link rel="stylesheet" href="css/flash.css">
+
     <title>HCMIU Hotel</title>
 </head>
 
 <body>
-    <!--  carousel -->
-    <section id="carouselExampleControls" class="carousel slide carousel_section" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="carousel-image" src="./image/image1.png">
-            </div>
-            <div class="carousel-item">
-                <img class="carousel-image" src="./image/image2.png">
-            </div>
-            <div class="carousel-item">
-                <img class="carousel-image" src="./image/image3.png">
-            </div>
-            <div class="carousel-item">
-                <img class="carousel-image" src="./image/image4.png">
+
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form method="POST">
+                <h1>Create Account</h1>
+                <input type="text" placeholder="Username" name="Username">
+                <input type="Email" placeholder="Email" name="Email">
+                <input type="Password" placeholder="Password" name="Password">
+                <input type="password" placeholder="Confirm Password" name="CPassword">
+                <button type="submit" name="user_signup_submit">Sign Up</button>
+            </form>
+        </div>
+			
+
+	<!-- // ==userlogin== -->
+
+        <div class="form-container sign-in">
+
+            <form id="userlogin" action="" method="POST">
+                <h1>Sign In</h1>
+                <input type="email" name="Email" placeholder="Email">
+                <input type="password" name="Password" placeholder="Password">
+                <button type="submit" name="user_login_submit">Sign In</button>
+            </form>
+        </div>
+
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Already have Account?</h1>
+                    <p>Sign in now to book room</p>
+                    <button class="hidden" id="login">Sign In</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Don't have Account?</h1>
+                    <p>Register with your email</p>
+                    <button class="hidden" id="register">Sign Up</button>
+                </div>
             </div>
         </div>
-    </section>
+    </div>
 
-    <!-- main section -->
-    <section id="auth_section">
-        <div class="logo" onclick="location.href='index.php';">
-            <img class="hcmiulogo" src="./image/hcmiulogo.png" alt="logo">
-            <p>HCMIU</p>
-        
-        </div>
-
-        <div class="auth_container">
-            <!--============ login =============-->
-
-            <div id="Log_in">
-                <h2>Log In</h2>
-                <!-- // ==userlogin== -->
+    <!-- // ==userlogin== -->
                 <?php 
+    
                 if (isset($_POST['user_login_submit'])) {
                     $Email = $_POST['Email'];
                     $Password = $_POST['Password'];
-
-                    $sql = "SELECT * FROM User WHERE Email = '$Email' AND Password = BINARY'$Password'";
+                    $sql = "SELECT * FROM User WHERE Email = '$Email' AND Password ='$Password'";
                     $result = mysqli_query($conn, $sql);
+            
+                    if ($result && $result->num_rows > 0) {
+                        $_SESSION['usermail'] = $Email;
 
-                    if ($result->num_rows > 0) {
-                        $_SESSION['usermail']=$Email;
-                        $Email = "";
-                        $Password = "";
                         header("Location: index.php");
+                        
                     } else {
                         echo "<script>swal({
                             title: 'Something went wrong',
@@ -78,65 +90,46 @@ session_start();
                         </script>";
                     }
                 }
-                ?>
-                <form class="user_login authsection active" id="userlogin" action="" method="POST">
-                    <div class="form-floating">
-                        <input typuser_logine="email" class="form-control" name="Email" placeholder=" ">
-                        <label for="Email">Email</label>
-                    </div>
-                    <div class="form-floating">
-                        <input type="password" class="form-control" name="Password" placeholder=" ">
-                        <label for="Password">Password</label>
-                    </div>
-                    <button type="submit" name="user_login_submit" class="auth_btn">Log in</button>
-
-                    <div class="footer_line">
-                        <h6>Don't have an account? <span class="page_move_btn" onclick="signuppage()">sign up</span></h6>
-                    </div>
-                </form>
-                
-                <!-- == Emp Login == -->
-            
-                
-            </div>
-
-            <!--============ signup =============-->
+                ?>  
+                 <!--============ signup =============-->
             <?php       
+
+
                 if (isset($_POST['user_signup_submit'])) {
                     $Username = $_POST['Username'];
                     $Email = $_POST['Email'];
                     $Password = $_POST['Password'];
                     $CPassword = $_POST['CPassword'];
-
-                    if($Username == "" || $Email == "" || $Password == ""){
+            
+                    if ($Username == "" || $Email == "" || $Password == "" || $CPassword == "") {
                         echo "<script>swal({
                             title: 'Fill the proper details',
                             icon: 'error',
                         });
                         </script>";
-                    }
-                    else{
-                        if ($Password == $CPassword) {
+                    } else {
+                        if ($Password==$CPassword) {
                             $sql = "SELECT * FROM User WHERE Email = '$Email' OR Username = '$Username'";
                             $result = mysqli_query($conn, $sql);
-    
-                            if ($result->num_rows > 0) {
+            
+                            if ($result ->num_rows > 0) {
                                 echo "<script>swal({
                                     title: 'Email/Username already exits',
                                     icon: 'error',
                                 });
                                 </script>";
                             } else {
-                                $sql = "INSERT INTO User (Username,Email,Password) VALUES ('$Username', '$Email', '$Password')";
+                                $sql = "INSERT INTO User (Username, Email, Password) VALUES ('$Username', '$Email', '$Password')";
                                 $result = mysqli_query($conn, $sql);
-    
+            
                                 if ($result) {
-                                    $_SESSION['usermail']=$Email;
+                                    $_SESSION['usermail'] = $Email;
                                     $Username = "";
                                     $Email = "";
                                     $Password = "";
                                     $CPassword = "";
                                     header("Location: index.php");
+                                   
                                 } else {
                                     echo "<script>swal({
                                         title: 'Something went wrong',
@@ -153,49 +146,9 @@ session_start();
                             </script>";
                         }
                     }
-                    
                 }
             ?>
-            <div id="sign_up">
-                <h2>Sign Up</h2>
-
-                <form class="user_signup" id="usersignup" action="" method="POST">
-                    <div class="form-floating">
-                        <input type="text" class="form-control" name="Username" placeholder=" ">
-                        <label for="Username">Username</label>
-                    </div>
-                    <div class="form-floating">
-                        <input type="email" class="form-control" name="Email" placeholder=" ">
-                        <label for="Email">Email</label>
-                    </div>
-                    <div class="form-floating">
-                        <input type="password" class="form-control" name="Password" placeholder=" ">
-                        <label for="Password">Password</label>
-                    </div>
-                    <div class="form-floating">
-                        <input type="password" class="form-control" name="CPassword" placeholder=" ">
-                        <label for="CPassword">Confirm Password</label>
-                    </div>
-
-                    <button type="submit" name="user_signup_submit" class="auth_btn">Sign up</button>
-
-                    <div class="footer_line">
-                        <h6>Already have an account? <span class="page_move_btn" onclick="loginpage()">Log in</span></h6>
-                    </div>
-                </form>
-            </div>
-    </section>
+    <script src="javascript/login.js"></script>
 </body>
 
-
-<script src="./javascript/index.js"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-<!-- aos animation-->
-<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-<script>
-    AOS.init();
-</script>
 </html>
-
